9063931b00dccf612bcfc2cf21dc720b2a41917b
Jason White <jasonaw0@gmail.com>
2014-08-07 15:36:32 -0700

    Convert graphviz and intmap tools to use Python 2.7.

--- tools/graphviz.py
+++ tools/graphviz.py
@@ -1,9 +1,14 @@
-#!/bin/env python3.4
+#!/bin/env python2.7
 """
 Generates a GraphViz file from *.graph and *.data files.
 
 Author: Jason White
 """
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+
 import sys
 import argparse
 
