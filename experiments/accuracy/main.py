1ca036cde486c5d796132b018fb751d08fa5ce93
Ali Hajimirza <a92hm@yahoo.com>
2014-08-06 17:39:50 -0700

    Accuracy scrip

---
+++ experiments/accuracy/main.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import os
+
+def read_ST_SSE(src):
+    budget = []
+    sse = []
+    sse_reduction = []
+    with open(src, 'rb') as error_file:
+        for line in error_file:
+            i, s, r = [float (a) for a in line.strip().split(',')]
+            budget.append(i)
+            sse.append(s)
+            sse_reduction.append(r)
+    budget = np.array(budget)
+    budget = np.multiply(budget, 16)
+    return np.array([budget,sse])
+
+def scatter_plot(data_arrays, labels, title, save=False):
+    plt.suptitle(title, fontsize=14)
+    plots = []
+    for data in data_arrays:
+        plot, = plt.plot(data[0], data[1])
+        plots.append(plot)
+    plt.legend(plots, labels, loc=2)
+    plt.xlabel('Bytes')
+    plt.ylabel('SSE')
+
+    if save:
+        plt.savefig(os.path.join(_PLOT_DEFAULT_PATH, title + '.png'))
+    else:
+        plt.show()
+    plt.clf()
+
+def main():
+    fourier_error = np.load('path/to/fourier/output.npy')[2]
+    fourier_error = np.array([np.arange(len(fourier_error))*12, fourier_error])
+    st_error = read_ST_SSE('path/to/ST/SLICE')
+
+    
+    scatter_plot([fourier_error, st_error], ['Fourier','ST'], 'Fourier_vs_ST_1000x5')
+    
+
+
+if __name__ == '__main__':
+    main()
