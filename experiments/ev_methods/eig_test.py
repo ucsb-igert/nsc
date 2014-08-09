a8c519033422d7ca13405df4cdf1be64cad0d987
Ali Hajimirza <a92hm@yahoo.com>
2014-08-09 13:34:35 -0700

    needs to be fixed

--- experiments/ev_methods/eig_test.py
+++ experiments/ev_methods/eig_test.py
@@ -14,6 +14,36 @@ import numpy as np
 import argparse
 import os
 
+def SSE_compress_graph(adjacency_matrix, node_value, parallel=True):
+    node_signal_values, eig_vecs = to_signal_domain(adjacency_matrix, node_value)
+    size = len(node_value)
+    sse = np.zeros((size, 3))
+    p = 0
+    for i in xrange(size):
+        p1 = int((i * 100.0)/size)
+        if p != p1:
+            p = p1
+            sys.stderr.write('\rReconstructing graph {}%'.format(p))
+        sse[i][t] = SSE(node_value, uncompress_method(node_signal_values, eig_vecs, type=t+1, count=i+1))
+    # Rearranging the values so each set of sse is in one array.
+    sys.stderr.write('\rReconstruction complete.\n')
+    return np.swapaxes(sse,0,1)
+
+def uncompress_method(node_signal_values, eig_vecs, type=3 ,count=None):
+    # Find the incidences of the highest f_signal and truncate the matrix
+    elements = np.argsort(node_signal_values)
+    if type == 1:
+        if count:
+            elements = elements[:count]
+    elif type == 2:
+        elements = elements[::-1]
+        if count:
+            elements = elements[:count]
+    elif type == 3:
+        if count:
+            elements = np.append(elements[:count/2], elements[-count/2:])
+    return to_graph_domain(node_signal_values[elements] ,eig_vecs[:,elements])
+
 _TMP_PATH = 'tmp'
 _RESULT_PATH = 'results'
 parser = argparse.ArgumentParser(description='Test SSE.')
