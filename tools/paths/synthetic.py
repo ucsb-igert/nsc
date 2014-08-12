c83ba67aa17e57ea39129f2e24d56f40c62d6162
Jason White <jason@kaweah.cs.uscb.edu>
2014-08-11 18:49:59 -0700

    Add list of reducted networks for synthetic.

--- tools/paths/synthetic.py
+++ tools/paths/synthetic.py
@@ -5,3 +5,7 @@ from glob import glob
 dir = join(data_dir, "Synthetic")
 data = join(dir, "synthetic.data")
 graph = join(dir, "synthetic.graph")
+
+reductions = {
+    1000: (join(dir, "synthetic.1000.data"), join(dir, "synthetic.1000.graph"))
+}
